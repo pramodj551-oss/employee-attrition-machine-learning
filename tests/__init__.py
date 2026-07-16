20ecae5007de79ef79a95c04e1e714aade11b816
Pramod prakash jadhav <pramodj551@gmail.com>
2026-07-16 15:26:05 +0530

    Create __init__.py
    git add tests/__init__.py
    git commit -m "Add tests package initialization"

---
+++ tests/__init__.py
@@ -0,0 +1,20 @@
+"""
+==========================================================
+Employee Attrition Machine Learning
+
+tests/__init__.py
+
+Author : Pramod Prakash Jadhav
+==========================================================
+
+This package contains unit tests for the Employee
+Attrition Prediction Machine Learning project.
+
+Modules
+-------
+- test_data_loader
+- test_preprocessing
+- test_feature_engineering
+- test_model_training
+- test_prediction
+"""
