69cb24ad6f1caed9e291158ad9b4211a394fc535
Pramod prakash jadhav <pramodj551@gmail.com>
2026-07-16 14:40:54 +0530

    Create __init__.py
    git add src/__init__.py
    git commit -m "Add source package initialization"

---
+++ src/__init__.py
@@ -0,0 +1,27 @@
+"""
+==========================================================
+Employee Attrition Machine Learning
+
+Source Package
+
+Author : Pramod Prakash Jadhav
+
+Description:
+This package contains all modules required for the
+Employee Attrition Machine Learning pipeline.
+
+Modules:
+- config
+- logger
+- data_loader
+- preprocessing
+- feature_engineering
+- model_training
+- evaluation
+- prediction
+- utils
+==========================================================
+"""
+
+__version__ = "1.0.0"
+__author__ = "Pramod Prakash Jadhav"
